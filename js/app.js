c42160116163ee83c9d656fed8a580670a4fe368
Jean Cabral <jean.souza@stone.com.br>
2021-02-27 20:48:43 -0300

    feat: :rocket: wip shopping cart

---
+++ js/app.js
@@ -0,0 +1,49 @@
+const produtos = document.querySelector('#lista-de-produto')
+
+cart = document.querySelector('#sacola tbody')
+
+
+loadEventListeners();
+
+function loadEventListeners() {
+    //escuta o evento de adicionar o carrinho
+    produtos.addEventListener('click', addtoCart);
+
+    function addtoCart(e) {
+        if (e.target.classList.contains('adicionar')) {
+            //pega os valores do produto
+            const item = e.target.parentElement.parentElement;
+            getProduct(item);
+        }
+    }
+}
+
+function getProduct(item) {
+    //cria o objeto
+    const produto = {
+        produto: item.querySelector('h4').textContent,
+        valor: item.querySelector('.preco span').textContent,
+        id: item.querySelector('a').getAttribute('data-id')
+    }
+    addToCart(produto);
+}
+
+function addToCart(item) {
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+        <tr>
+            <td>
+                ${item.produto}
+            </td>
+            <td>
+                ${item.valor}
+            </td>
+            <td>
+                <a href="#" class="remove" data-id="${item.id}">X</a>
+            </td>
+        </tr>
+    `;
+
+    cart.appendChild(row);
+}
